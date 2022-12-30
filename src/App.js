11faca04bbca67d9ccd5e5732c0fc15b6dfd54c0
jishna-roshy <jishnaroshy9865@gmail.com>
2022-12-30 12:35:20 +0530

    neflix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import Row from './components/Row';
 import './App.css';
+import requests from './request';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+<Nav/>
       </header>
+      <Banner  fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row 
+      isLargeRow={true}
+      title="Netflix Orginals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+
+
     </div>
   );
 }
---
+++ src/components/Row.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import instance from '../baseUrl'
+import './Row.css'
+
+
+
+function Row({ title, fetchUrl, isLargeRow }) {
+
+    //movies state
+    const [movies, setMovies] = useState([])
+    async function fetchData() {
+        const result = await instance.get(fetchUrl)
+        console.log(result.data.results);
+        setMovies(result.data.results);
+        console.log(movies);
+
+
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+    const base_url = "https://image.tmdb.org/t/p/original";
+    return (
+        <div className='row'>
+            <h2>{title} </h2>
+            <div className='movies'>
+                {
+                    movies.map(movie => (
+                        <img className={`movie ${isLargeRow && "largeMovie"}`}
+                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            alt={movie.name}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Row
